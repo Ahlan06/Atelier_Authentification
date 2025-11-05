639fcf974276518da3b66458241802b164453105
Ahlan Mira <ahlan.mira@icloud.com>
2025-11-05 11:25:50 +0100

    Implement logout functionality with session destruction

--- atelier2/logout.php
+++ atelier2/logout.php
@@ -1,6 +1,12 @@
 <?php
+// Démarrer la session
+session_start();
+
 // Supprime le cookie d'authentification
-setcookie('authToken', '', time() - 3600, '/', '', false, true); // Cookie expiré par la valeur -3600. Il n'est donc plus valable
+setcookie('authToken', '', time() - 3600, '/', '', false, true);
+
+// Détruire la session et le token
+session_destroy();
 
 // Redirection vers la page de connexion
 header('Location: index.php');
